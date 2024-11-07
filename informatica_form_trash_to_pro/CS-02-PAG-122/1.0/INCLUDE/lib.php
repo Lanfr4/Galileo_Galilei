2a578649b264fa6fb06d6c3bbeea5021c63f9625
Sebastiano Lanfranchi <128606572+Lanfr4@users.noreply.github.com>
2024-11-07 11:49:17 +0100

    Add files via upload

---
+++ informatica_form_trash_to_pro/CS-02-PAG-122/1.0/INCLUDE/lib.php
@@ -0,0 +1,56 @@
+<?php
+$DBHOST = "localhost";
+$DBUSER = "root";
+$DBPASSWORD = "root";
+$DBNAME = "cs02pag122"; // cs02pag122 se lo avete chiamato così!
+
+function writeHeader(){
+    echo('
+        <!DOCTYPE HTML>
+            <html>
+                <head>
+                    <meta charset="utf-8">
+                    <title>CASO STUDIO 02 PAGINA 122</title>
+                    <meta name="viewport" content="width=device-width, initial-scale=1">
+                    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+                </head>
+                <body>
+                    <div class="container">
+    ');
+    return;
+}
+
+function writeFooter(){
+    echo('
+                    </div>
+                    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></script>
+                    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.min.js" integrity="sha384-0pUGZvbkm6XF6gxjEnlmuGrJXVbNuzT9qBBavbLwCsOGabYfZo0T0to5eqruptLy" crossorigin="anonymous"></script>
+                </body>
+            </html>');
+    return;
+}
+
+function writeMenu(){
+    echo('
+        <nav class="navbar navbar-expand-lg bg-body-tertiary">
+            <div class="container-fluid">
+                <a class="navbar-brand" href="#">HOME</a>
+                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                        <li class="nav-item dropdown">
+                            <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                                PRODOTTO
+                            </a>
+                            <ul class="dropdown-menu">
+                                <li><a class="dropdown-item" href="prodotto.php?scelta=formProdotto">Add Prodotto</a></li>
+                                <li><a class="dropdown-item" href="prodotto.php?scelta=listaProdotto">List Prodotto</a></li>
+                            </ul>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </nav>    
+    ');
+    return;
+}
+?>
