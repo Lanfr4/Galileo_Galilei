724ae9e8c9af2ab2e4c703ef491e48f41feba260
Sebastiano Lanfranchi <128606572+Lanfr4@users.noreply.github.com>
2024-11-11 10:46:29 +0000

    upd

--- informatica_form_trash_to_pro/CS-02-PAG-122/1.0/PHP/prodotto.php
+++ informatica_form_trash_to_pro/CS-02-PAG-122/1.0/PHP/prodotto.php
@@ -1,56 +1,76 @@
 <?php
-if(isset($_REQUEST['scelta']))  $sc = $_REQUEST['scelta']; else $sc = null;
-// includo la libreria con funzioni necessarie all' interfaccia.
+/** Questa pagina sarà adibita alla gestione della tabella PRODOTTO */
+
+if(isset($_REQUEST['scelta'])) $sc = $_REQUEST['scelta']; else $sc = null;
+
+// includo la libreria con funzioni necessarie all'interfaccia.
 require('../include/lib.php');
-//riciamo la funzione per la creazine della sezione iniziale delle pagine 
+// richiamo la funzione per la creazione della sezione iniziale delle pagine
 writeHeader();
 writeMenu();
 
-    //<<your code starts here>>
+    // << your code start here >>
         switch($sc){
             case "listaProdotto":{
-                echo('Contenuto della Tabella Prodotto: <br />');
-                // questo case deve aprire un flusso con mySQL, qundi eseguire
-                // una query per visualizzare il contenuto della tabella PRODOTTO, con o senza tabella bootstrap
+                //echo('Contenuto della tabella Prodotto:<br />');
+                // questo case deve aprire un flusso con mysql, quindi eseguire una query
+                // per visualizzare il contenuto della tabella PRODOTTO, con o senza tabella bootstrap.
                 $db = new mysqli($DBHOST, $DBUSER, $DBPASSWORD, $DBNAME);
                 $sql = "SELECT * FROM prodotto";
-                $risultSet = $db->query($sql);
-                
-                echo('<table class="table">
-                    <thead>    
-                            <tr>
-                                <th scope="col">#</th>
-                                <th scope="col">Desrizione</th>
-                            </tr>
-                    </thead>
-                ');
-                /*while($record = $risultSet->fetch_assoc()){
-            
-                    echo($record[id].' '.$record['descrizione'].'<br />');
-                }
-                $db->close();
-                break;*/
-            }
+                $resultSet = $db->query($sql);
 
-            case "formNuovoProdotto":{
-                //crea il form HTML/Bootstrap per l' inserimento dei dati di un prodotto
-                echo('
-                    form action ="prodotto.php">
-                        <div class="mb-3">
-                            <label for="exampleInputDescrizione" class="form-label">Descrizione Prodotto</label>
-                            <input type="text" class="form-control" id="inputDescrizione" aria-describedby="Descrizione Help">
-                            <div id="descrizioneHelp" class="form-text">Inserisci la descrizione del prodotto da inserire.</div>
-                        </div>
-                        <inpput type="hidden" name"scelta" value ="aggiungiProdotto">
-                        <button type="submit" class="btn btn-primary">Submit</button>
-                    </form>
-                    ');
+                /*while($record = $resultSet->fetch_assoc()){
+                    echo($record['id'].' '.$record['descrizione'].'<br />');    
+                }*/
+                echo('<table class="table table-striped table-hover ">
+                    <caption>Lista dei prodotti disponibili</caption>
+                    <thead>
+                        <tr>
+                            <th scope="col">#</th>
+                            <th scope="col">Descrizione</th>
+                        </tr>
+                    </thead>
+                    <tbody>                
+                ');
+                    while($record = $resultSet->fetch_assoc()){
+                        echo('<tr>
+                                <th scope="row">'.$record['id'].'</th>
+                                <td>'.$record['descrizione'].'</td>
+                            </tr>
+                        ');  
+                    }
+                echo('</tbody>
+                </table>');
+
+                /* per poter veddere di nuovo i risultati devo ri-eseguire di nuovo la query 
+                    e ri-creare di nuovo la resultSet
+                echo('<br /><br />');
+                $resultSet = $db->query($sql);
+                while($record = $resultSet->fetch_assoc()){
+                    echo($record['id'].' '.$record['descrizione'].'<br />');    
+                }
+                */
+                $db->close();
                 break;
             }
-
+            case "formNuovoProdotto":{
+                // crea il form HTML/Bootstrap per l'inserimento dei dati di un prodotto.
+                echo('
+                    <form action="prodotto.php">
+                        <div class="mb-3">
+                            <label for="inputDescrizione" class="form-label">Descrizione Prodotto:</label>
+                            <input type="text" name="descrizione" class="form-control" id="inputDescrizione" aria-describedby="descrizioneHelp">
+                            <div id="descrizioneHelp" class="form-text">Inserisci la descrizione del prodotto da inserire</div>
+                        </div>
+                        <input type="hidden" name="scelta" value="aggiungiProdotto">
+                        <button type="submit" class="btn btn-primary">Submit</button>
+                    </form>
+                ');
+                break;
+            }
             case "aggiungiProdotto":{
-                /* Case richiamnto dal form di inserimento nuovo prodotto, quindi
-                    nel vettore $_REQUEST[] avro la chiave 'descrizione' con contenuto il testo immesso
+                /* Case richiamato dal form di inserimento nuovo prodotto, quindi
+                    nel vettore $_REQUEST[] avrò la chiave 'descrizione' con contenuto il testo immesso
                     nella casella del form
                 */
                 $desc = $_REQUEST['descrizione'];
@@ -58,16 +78,17 @@ writeMenu();
 
                 $sql = "INSERT INTO prodotto(descrizione) VALUES('$desc')";
                 if($db->query($sql)){
-                    echo('<div class="alert alert-success"> Nuovo prodotto aggiunto.</div>');
+                    echo('<div class="alert alert-success">Nuovo prodotto aggiunto.</div>');
                 }
                 else{
-                    echo('<div class="alert alert-warning"> Problema in aggiunta nuovo prodotto.</div>');
+                    echo('<div class="alert alert-warning">Problema in aggiunta nuovo prodotto.</div>');
                 }
+                $db->close();
                 break;
             }
         }
-    //<< end for your code >>
-    
-//richiamo la funzione per la creazione della sezione finale delle pagine
-writeFooter(); 
+    // << end fo your code >>
+
+// richiamo la funzione per la creazione della sezione finale delle pagine.
+writeFooter();
 ?>
