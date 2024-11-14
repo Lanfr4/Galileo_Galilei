5adf3ea7d24d8bfc36089c6b3cad3bf444bf04e9
Sebastiano Lanfranchi <128606572+Lanfr4@users.noreply.github.com>
2024-11-14 12:09:15 +0100

    Add files via upload

--- informatica_form_trash_to_pro/CS-02-PAG-122/1.0/INCLUDE/lib.php
+++ informatica_form_trash_to_pro/CS-02-PAG-122/1.0/INCLUDE/lib.php
@@ -1,9 +1,15 @@
 <?php
+/* Questo file contiene tutte le funzioni utili e che saranno richiamate nelle diverse pagine,
+il file sarà incluso nei file php che si trovano in /php. */
 $DBHOST = "localhost";
 $DBUSER = "root";
 $DBPASSWORD = "root";
 $DBNAME = "cs02pag122"; // cs02pag122 se lo avete chiamato così!
 
+/**
+ * Funzione che genera la parte iniziale di ogni pagina, costruendo il corpo della pagina partendo dai tag fondamentali, genera il codice fino
+ * alla DIV generale che conterrà tutta l'interfaccia predisposta nelle diverse pagine di gestione.
+ */
 function writeHeader(){
     echo('
         <!DOCTYPE HTML>
@@ -20,6 +26,9 @@ function writeHeader(){
     return;
 }
 
+/**
+ * Funzione che genera le righe di chiusura di ogni pagina così da chiudere in modo corretto ogni interfaccia.
+ */
 function writeFooter(){
     echo('
                     </div>
@@ -30,11 +39,17 @@ function writeFooter(){
     return;
 }
 
+/**
+ * Funzione che genera il codice HTML e Bootstrap per rappresentare una barra di navigazione ad inizio pagina o comunque dove viene richiamata.
+ */
 function writeMenu(){
     echo('
         <nav class="navbar navbar-expand-lg bg-body-tertiary">
             <div class="container-fluid">
                 <a class="navbar-brand" href="#">HOME</a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
                     <ul class="navbar-nav me-auto mb-2 mb-lg-0">
                         <li class="nav-item dropdown">
@@ -42,15 +57,24 @@ function writeMenu(){
                                 PRODOTTO
                             </a>
                             <ul class="dropdown-menu">
-                                <li><a class="dropdown-item" href="prodotto.php?scelta=formProdotto">Add Prodotto</a></li>
+                                <li><a class="dropdown-item" href="prodotto.php?scelta=formNuovoProdotto">Add Prodotto</a></li>
                                 <li><a class="dropdown-item" href="prodotto.php?scelta=listaProdotto">List Prodotto</a></li>
+                            </ul>
+                         <li class="nav-item dropdown">
+                            <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                                MAGAZZINO
+                            </a>
+                            <ul class="dropdown-menu">
+                                <li><a class="dropdown-item" href="magazzino.php?scelta=formNuovoMagazzino">Add Magazzino</a></li>
+                                <li><a class="dropdown-item" href="magazzino.php?scelta=listaMagazzino">List Magazzino</a></li>
                             </ul>
                         </li>
                     </ul>
                 </div>
             </div>
         </nav>    
-    ');
+    '); //*/
+
     return;
 }
 ?>
