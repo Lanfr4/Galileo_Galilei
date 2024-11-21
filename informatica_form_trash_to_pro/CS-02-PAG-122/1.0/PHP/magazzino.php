ffa5486c180655799d10a899656384eaf734b6f1
Lucifersiss-debug <riccardobose06@gmail.com>
2024-11-21 10:16:21 +0100

    SUCA_magazzino

--- informatica_form_trash_to_pro/CS-02-PAG-122/1.0/PHP/magazzino.php
+++ informatica_form_trash_to_pro/CS-02-PAG-122/1.0/PHP/magazzino.php
@@ -1,123 +1,105 @@
 <?php
-/** Questa pagina sarà adibita alla gestione della tabella PRODOTTO */
-
+    
 if(isset($_REQUEST['scelta'])) $sc = $_REQUEST['scelta']; else $sc = null;
 
-// includo la libreria con funzioni necessarie all'interfaccia.
 require('../include/lib.php');
-// richiamo la funzione per la creazione della sezione iniziale delle pagine
+
 writeHeader();
 writeMenu();
 
-    // << your code start here >>
-        switch($sc){
-            case "listaMagazzino":{
-                //echo('Contenuto della tabella Prodotto:<br />');
-                // questo case deve aprire un flusso con mysql, quindi eseguire una query
-                // per visualizzare il contenuto della tabella PRODOTTO, con o senza tabella bootstrap.
-                $db = new mysqli($DBHOST, $DBUSER, $DBPASSWORD, $DBNAME);
-                $sql = "SELECT * FROM magazzino";
-                $resultSet = $db->query($sql);
+    switch($sc){
+        case "listaMagazzino":{
+            $db = new mysqli($DBHOST, $DBUSER, $DBPASSWORD, $DBNAME);
+            $sql = "SELECT * FROM magazzino";
+            $resultSet = $db->query($sql);
 
-                /*while($record = $resultSet->fetch_assoc()){
-                    echo($record['id'].' '.$record['descrizione'].'<br />');    
-                }*/
-                echo('<table class="table table-striped table-hover ">
-                    <caption>Lista dei magazzino disponibili</caption>
-                    <thead>
-                        <tr>
-                            <th scope="col">#</th>
-                            <th scope="col">nome</th>
-                            <th scope="col"> citta</th>
-                            <th scope="col">provincia</th>
-                            <th scope="col">Gestione</th>
-                        </tr>
-                    </thead>
-                    <tbody>                
-                ');
-                    while($record = $resultSet->fetch_assoc()){
-                        echo('<tr>
-                                <th scope="row">'.$record['id'].'</th>
-                                <td>'.$record['nome'].'</td>
-                                <td>'.$record['citta'].'</td> 
-                                <td>'.$record['provincia'].'</td>
-                                <td><a href="magazzino.php?scelta=deleteMagazzino&idMagazzino='.$record['id'].'">
-                                        <button type="button" class="btn btn-primary">Cancella</button>
-                                    </a>
-                                </td>
-                            </tr>
-                        ');  
-                    }
-                echo('</tbody>
-                </table>');
-
-                /* per poter veddere di nuovo i risultati devo ri-eseguire di nuovo la query 
-                    e ri-creare di nuovo la resultSet
-                echo('<br /><br />');
-                $resultSet = $db->query($sql);
+            echo('<table class="table table-striped table-hover ">
+                <caption>Lista dei magazzino</caption>
+                <thead>
+                    <tr>
+                        <th scope="col">#</th>
+                        <th scope="col">Nome</th>
+                        <th scope="col">Citta</th>
+                        <th scope="col">Provincia</th>
+                        <th scope="col">Gestione</th>
+                    </tr>
+                </thead>
+                <tbody>                
+            ');
                 while($record = $resultSet->fetch_assoc()){
-                    echo($record['id'].' '.$record['descrizione'].'<br />');    
+                    echo('<tr>
+                            <th scope="row">'.$record['id'].'</th>
+                            <td>'.$record['nome'].'</td>
+                            <td>'.$record['citta'].'</td>
+                            <td>'.$record['provincia'].'</td>
+                            <td><a href="magazzino.php?scelta=deleteMagazzino&idMagazzino='.$record['id'].'">
+                                    <button type="button" class="btn btn-primary">Cancella</button>
+                                </a>
+                            </td>
+                        </tr>
+                    ');  
                 }
-                */
-                $db->close();
-                break;
-            }
-            case "formNuovoMagazzino":{
-                // crea il form HTML/Bootstrap per l'inserimento dei dati di un prodotto.
-                echo('
-                    <form action="magazzino.php">
-                        <div class="mb-3">
-                            <label for="inputDescrizione" class="form-label">nome Magazzino:</label>
-                            <input type="text" name="nome" class="form-control" id="inputnome" aria-describedby="nomeHelp">
-                            <label for="inputDescrizione" class="form-label">citta Magazzino:</label>
-                            <input type="text" name="citta" class="form-control" id="inputcitta" aria-describedby="cittaHelp">
-                            <label for="inputDescrizione" class="form-label">provincia citta Magazzino:</label>
-                            <input type="text" name="provincia" class="form-control" id="inputprovincia" aria-describedby="provinciaHelp">
-                        </div>
-                        <input type="hidden" name="scelta" value="aggiungiMagazzino">
-                        <button type="submit" class="btn btn-primary">Submit</button>
-                    </form>
-                ');
-                break;
-            }
-            case "aggiungiMagazzino":{
-                /* Case richiamato dal form di inserimento nuovo prodotto, quindi
-                    nel vettore $_REQUEST[] avrò la chiave 'descrizione' con contenuto il testo immesso
-                    nella casella del form
-                */
-                $desc = $_REQUEST['nome'];
-                $citta = $_REQUEST['citta'];
-                $provincia = $_REQUEST['provincia'];
+            echo('</tbody>
+            </table>');
+
+            $db->close();
+            break;
+        }
+        case "formNuovoMagazzino":{
+            // crea il form HTML/Bootstrap per l'inserimento dei dati di un prodotto.
+            echo('
+                <form action="magazzino.php">
+                    <div class="mb-3">
+                        <label for="inputNome" class="form-label">Nome Magazzino:</label>
+                        <input type="text" name="nome" class="form-control" id="inputNome" aria-describedby="nomeHelp">
+                        <div id="nomeHelp" class="form-text">Inserisci il nome del magazzino</div>
+                        <label for="inputCitta" class="form-label">Citta Magazzino:</label>
+                        <input type="text" name="citta" class="form-control" id="inputCitta" aria-describedby="cittaHelp">
+                        <div id="cittaHelp" class="form-text">Inserisci la citta del magazzino</div>
+                        <label for="inputProvincia" class="form-label">Provincia Magazzino:</label>
+                        <input type="text" name="provincia" class="form-control" id="inputProvincia" aria-describedby="provinciaHelp">
+                        <div id="provinciaHelp" class="form-text">Inserisci la provincia del magazzino</div>
+                    </div>
+                    <input type="hidden" name="scelta" value="aggiungiMagazzino">
+                    <button type="submit" class="btn btn-primary">Submit</button>
+                </form>
+            ');
+            break;
+        }
+        case "aggiungiMagazzino" : {
+
+            $nome = $_REQUEST['nome'];
+            $citta = $_REQUEST['citta'];
+            $provincia = $_REQUEST['provincia'];
                 $db = new mysqli($DBHOST, $DBUSER, $DBPASSWORD, $DBNAME);
 
-                $sql = "INSERT INTO magazzino(nome, citta, provincia) VALUES('$desc', '$citta', '$provincia')";
-
-
+                $sql = "INSERT INTO magazzino(nome,citta,provincia) VALUES('$nome','$citta','$provincia')";
                 if($db->query($sql)){
                     echo('<div class="alert alert-success">Nuovo magazzino aggiunto.</div>');
                 }
                 else{
-                    echo('<div class="alert alert-warning">Problema in aggiunta nuovo magazzino.</div>');
+                    echo('<div class="alert alert-warning">Problema in aggiunta del nuovo magazzino.</div>');
                 }
                 $db->close();
                 break;
-            }
-            case "deleteMagazzino":{
-                $idP = $_REQUEST['idMagazzino'];
-                $db = new mysqli($DBHOST, $DBUSER, $DBPASSWORD, $DBNAME);
+         }
+         case "deleteMagazzino":{
+            $idP = $_REQUEST['idMagazzino'];
+            $db = new mysqli($DBHOST, $DBUSER, $DBPASSWORD, $DBNAME);
 
-                $sql = "DELETE FROM magazzino WHERE id='$idP'";
-                if($db->query($sql))
-                    echo('<div class="alert alert-success">Magazzino Cancellato Correttamente.</div>');
-                else
-                    echo('<div class="alert alert-warning">Magazzino inesistente o errore in cancellazione.</div>');
-                
-                $db->close();
-                break;
-            }
+            $sql = "DELETE FROM magazzino WHERE id='$idP'";
+            if($db->query($sql))
+                echo('<div class="alert alert-success">Magazzino Cancellato Correttamente.</div>');
+            else
+                echo('<div class="alert alert-warning">Magazzino inesistente o errore in cancellazione.</div>');
+            
+            $db->close();
+            break;
         }
-    // << end fo your code >>
 
-// richiamo la funzione per la creazione della sezione finale delle pagine.
+    }
+
 writeFooter();
+
+
 ?>
