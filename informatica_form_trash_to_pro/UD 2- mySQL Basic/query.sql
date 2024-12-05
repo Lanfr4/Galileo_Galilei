9f8e21908aecd3f99de31379c7a7838ac08d8f3a
Sebastiano Lanfranchi <128606572+Lanfr4@users.noreply.github.com>
2024-12-05 11:33:21 +0100

    Add files via upload

--- informatica_form_trash_to_pro/UD 2- mySQL Basic/query.sql
+++ informatica_form_trash_to_pro/UD 2- mySQL Basic/query.sql
@@ -1,38 +1,65 @@
--- Questo file contiene tutte le query(INTEROGAZIONE FATTE AL DATABASE) sviluppate eseguibili
--- sulla banca dati in uso
+-- Visualizzare tutti i magazzini ordinati per provincia :)
+SELECT * 
+FROM  magazzino
+ORDER BY provincia asc;
+-- asc  sta per ascending
+-- desc sta per descending
 
---Q1 : Visualizzare tutti gli artisti presenti nel database
-SELECT * FROM Artista;
+-- Visualizzare tutti gli ordini presenti a DataBase :)
+SELECT * 
+FROM acquisto
+ORDER BY dataAcquisto
 
---Q1.1 : 
-Select nome, cognome, nazione FROM Artista
+-- Visualizzare tutti gli ordini presenti a DataBase includendo mdati del prodotto e del relativo magazzino 
+SELECT p.descrizione, m.nome, m.citta, m.provincia, a.dataAcquisto, a.quantitaAcquisto, a.prezzoAcquisto
+FROM prodotto as p, magazzino as m, acquisto as a;
+WHERE p.id = a.idPrdodotto AND  a.idMagazzino = m.id;
 
--- Q1.2
-Select  cognome, nome, nazione FROM Artista
+-- QUERY fatta con Inner Join :)
+SELECT p.descrizione, m.nome, m.citta, m.provincia, a.dataAcquisto, a.quantitaAcquisto, a.prezzoAcquisto
+FROM prodotto as 
+INNER JOIN prodotto as p on p.id = a.idPrdodotto
+INNER JOIN magazzino as m on a.idMagazzino = m.id
+ORDER BY a.dataAcquisto, a.prezzoAcquisto desc;
 
---Q2: Visualizzare tutti i brani preenti .
-SELECT * FROM Brano;
+-- Determinare il numero di prodotto presenti a DATABASE
+-- Per fare questo mySQL ci mette a disposizione un operatore specifico
+SELECT count(id)
+FROM prodotto
 
---Q2.1
-SELECT Brano.*, Artista.*
-FROM Brano, Artista;
+--Determinare il prezzo minore dei prodotti presenti nel DATBASE
+-- Viene fatta con la select
+SELECT min(prezzoAcquisto)
+FROM prodotto
 
---Q2.2
-SELECT Brano.*, Artista.*
-FROM Brano, Artista
-WHERE Brano.idArtista = Artista.id;
+--Determinare il prezzo massimo di acquisto del DATABASE
+SELECT max(prezzoAcquisto)
+FROM acquisto
 
---Q3 : Visualizzare tutti i brani e relativo autore, ordinando 
---i risultati per cognome dell' autore
-SELECT Brano.*, Artista.*
-FROM Brano, Artista
-WHERE Brano.idArtista = Artista.id
-ORDER BY Artista.cognome;
+--Determinare il prezzo medio degli acquisti presenti sul DATABASE
+-- Utilizzando AVARAGE (media) sintetizzandolo con avg 
+--LIMIT serve per limitare il numero di risultati visualizzati dalla query
+SELECT avg(prezzoAcquisto)
+FROM acquisto
 
---Q3.2
-SELECT Brano.*, Artista.*
-FROM Brano, Artista
-INNER JOIN Artista = Artista.id
-ON Brano.idArtista = Artista.id
-ORDER BY Artista.cognome;
+-- Visualizzare tutti gli acquisti con prezzo inferiore o uguale al prezzo di acquisto medio
+SELECT acquisto.* -- tiro su tutti i campo di acquisto
+FROM acquisto
+WHERE prezzoAcquisto <= ( 
+    SELECT avg(prezzoAcquisto) FROM acquisto
+    );
 
+-- Come query precedente ma visualizzando i dati del prodotto
+SELECT p.decrizione as prodotto, a.dataAcquisto , a.quantitaAcquista, a.prezzoAcquisto
+FROM acquisto as a, prodotto as p, magazzino as m
+WHERE a.prezzoAcquisto <= (
+    SELECT avg(prezzoAcquisto) FROM acquisto);
+AND p.id == idProdotto 
+AND a.idMagazzino = m.id;
+
+--Visualizzare tutti gli ordini effettuati dal magazzino  'Mantova 1';
+SELECT p.descrizione, a.dataAcquisto, a.prezzoAcquisto, a.quantitaAcquisto
+FROM prodotto as p, acquisto as a, magazzino as m
+WHERE p.id = a.idProdotto
+AND a.idMagazzino = m.id
+AND  m.nome = 'Mantova 1'; -- anche cambiando tra min e maius le lettere non cambia pk in questo caso il databes e case insensitive :)
