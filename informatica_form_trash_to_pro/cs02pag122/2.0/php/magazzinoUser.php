b5eebc1e5aea690d0ac5622e96f3a31a46c73f45
Sebastiano Lanfranchi <128606572+Lanfr4@users.noreply.github.com>
2025-02-05 20:20:29 +0100

    Add files via upload

--- informatica_form_trash_to_pro/cs02pag122/2.0/php/magazzinoUser.php
+++ informatica_form_trash_to_pro/cs02pag122/2.0/php/magazzinoUser.php
@@ -1,9 +1,6 @@
 <?php
 session_start();
-/* Questa pagina sarà adibita al FRONTEND del progetto
- * ovvero una pagina dedicata alla visualizzazione di un utente non loggato
-*/
-if(!isset($_SESSION['logged'])) $_SESSION['logged'] = false;
+/** Questa pagina sarà adibita alla gestione della tabella PRODOTTO */
 if(isset($_REQUEST['scelta'])) $sc = $_REQUEST['scelta']; else $sc = null;
 
 // includo la libreria con funzioni necessarie all'interfaccia.
@@ -14,45 +11,13 @@ writeHeader();
 
     // << your code start here >>
         switch($sc){
-            case "listaProdotto":{
-    
-                $db = new mysqli($DBHOST, $DBUSER, $DBPASSWORD, $DBNAME);
-                $sql = "SELECT * FROM prodotto";
-                $resultSet = $db->query($sql);
-
-                echo('<table class="table table-striped table-hover ">
-                    <caption>Lista dei prodotti disponibili</caption>
-                    <thead>
-                        <tr>
-                            <th scope="col">#</th>
-                            <th scope="col">Descrizione</th>
-                        </tr>
-                    </thead>
-                    <tbody>                
-                ');
-                    while($record = $resultSet->fetch_assoc()){
-                        echo('<tr>
-                                <th scope="row">'.$record['id'].'</th>
-                                <td>'.$record['descrizione'].'</td>
-                                <td><a href="prodottoUser.php?scelta=deleteProdotto&idProdotto='.$record['id'].'">
-                                    </a>
-                                </td>
-                            </tr>
-                        ');  
-                    }
-                echo('</tbody>
-                </table>');
-
-                $db->close();
-                break;
-            }
             case "listaMagazzino":{
                 $db = new mysqli($DBHOST, $DBUSER, $DBPASSWORD, $DBNAME);
                 $sql = "SELECT * FROM magazzino";
                 $resultSet = $db->query($sql);
 
                 echo('<table class="table table-striped table-hover ">
-                    <caption>Lista dei prodotti disponibili</caption>
+                    <caption>Lista dei Magazzini disponibili</caption>
                     <thead>
                         <tr>
                             <th scope="col">#</th>
@@ -69,8 +34,6 @@ writeHeader();
                                 <td>'.$record['nome'].'</td>
                                 <td>'.$record['citta'].'</td>
                                 <td>'.$record['provincia'].'</td>
-                                <td><a href="magazzinoUser.php?scelta=deleteMagazzino&idMagazzino='.$record['id'].'">
-                                    </a>
                                 </td>
                             </tr>
                         ');  
@@ -80,6 +43,7 @@ writeHeader();
                 $db->close();                
                 break;
             }
+                
         }
     // << end fo your code >>
 // richiamo la funzione per la creazione della sezione finale delle pagine.
