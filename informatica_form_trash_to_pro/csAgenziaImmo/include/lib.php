6e7bad8b8ac8a578ec1d4b4c458236f9a0b78f1a
Lanfr4_ <sebalanfro@gmail.com>
2025-02-20 21:22:00 +0100

    modifiche

--- informatica_form_trash_to_pro/csAgenziaImmo/include/lib.php
+++ informatica_form_trash_to_pro/csAgenziaImmo/include/lib.php
@@ -12,7 +12,43 @@ $DBNAME ="csAgenzia";
  * alla DIV generale che conterrà tutta l'interfaccia predisposta nelle diverse pagine di gestione.
  */
 
+ function writeHeader(){
+    echo('
+        <!DOCTYPE HTML>
+            <html>
+                <head>
+                    <meta charset="utf-8">
+                    <title>CASO STUDIO 02 PAGINA 122</title>
+                    <meta name="viewport" content="width=device-width, initial-scale=1">
+                    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+                </head>
+                <body>
+                    <div class="container">
+    ');
+    return;
+}
 
+/**
+ * Funzione che genera le righe di chiusura di ogni pagina così da chiudere in modo corretto ogni interfaccia.
+ */
+function writeFooter(){
+    echo('
+                    </div>
+                    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></script>
+                    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.min.js" integrity="sha384-0pUGZvbkm6XF6gxjEnlmuGrJXVbNuzT9qBBavbLwCsOGabYfZo0T0to5eqruptLy" crossorigin="anonymous"></script>
+                </body>
+            </html>');
+    return;
+}
 
+/**
+ * Funzione che genera il codice HTML e Bootstrap per rappresentare una barra di navigazione ad inizio pagina o comunque dove viene richiamata.
+ */
+function writeMenu(){
+    echo('
+         
+    '); //*/
 
+    return;
+}
 ?>
