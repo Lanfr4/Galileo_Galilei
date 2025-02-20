45a6d03e32f0bf24dd54d70f715283c8e9d52532
Lanfranchi Sebastiano <sebastiano.lanfranchi@galilei.intranet>
2025-02-20 11:25:41 +0100

    ciao

---
+++ informatica_form_trash_to_pro/csAgenziaImmo/include/lib.php
@@ -0,0 +1,18 @@
+<?php
+/* Questo file contiene tutte le funzioni utili e che saranno richiamate nelle diverse pagine,
+il file sarà incluso nei file php che si trovano in /php. */
+
+$DBHOST:"localhost";
+$DBUSER:"root";
+$DBPASSWORD"";
+$DBNAME:"csAgenzia";
+
+/**
+ * Funzione che genera la parte iniziale di ogni pagina, costruendo il corpo della pagina partendo dai tag fondamentali, genera il codice fino
+ * alla DIV generale che conterrà tutta l'interfaccia predisposta nelle diverse pagine di gestione.
+ */
+
+
+
+
+?>
