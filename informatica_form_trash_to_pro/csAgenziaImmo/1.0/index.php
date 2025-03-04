ee779ffd3cb0e4c541faa2b5083d619754002a09
Lanfr4_ <sebalanfro@gmail.com>
2025-03-04 16:54:53 +0100

    update

--- informatica_form_trash_to_pro/csAgenziaImmo/1.0/index.php
+++ informatica_form_trash_to_pro/csAgenziaImmo/1.0/index.php
@@ -1,6 +1,6 @@
 <?php
 // questo file è nella root della versione in sviluppo e esegue
 // un redirect alla cartella del core PHP da usare.
-header("location: ./php");
+echo("location: ../php");
 exit;
 ?>
