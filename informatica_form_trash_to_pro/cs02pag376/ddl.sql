d02391b4deeb151bf43d0a06bc6ab85fb420d6bf
Lanfr4_ <sebalanfro@gmail.com>
2025-05-05 16:46:17 +0200

    upd

--- informatica_form_trash_to_pro/cs02pag376/ddl.sql
+++ informatica_form_trash_to_pro/cs02pag376/ddl.sql
@@ -1,1 +1,30 @@
+CREATE TABLE REGIONE(
+    id integer unsigned auto_increment,
+    descrizione char(30) not null,
 
+    primary key(id)
+)ENGINE =InnoDB;
+
+CREATE TABLE TIPO_S(
+    id integer unsigned auto_increment,
+    descrizione char(30) not null,
+
+    primary key(id)
+)ENGINE = InnoDB;
+
+CREATE TABLE STRUTTURA(
+    id integer unsigned auto_increment,
+    località varchar(20) not null,
+    provincia char(20) not null,
+    indirizzo varchar(30) not null,
+    telefono integer not null,
+    email varchar(20) not null,
+    foto,
+    soggiorno float not null,
+    idRegione integer,
+    idTipo_S integer,
+
+    primary key(id),
+    foreign key idRegione references REGIONE(id),
+    foreign key idTipo_S references  TIPO_S(id)
+)ENGINE = InnoDB;
